edaa216396aa9edc90d3c38201081791c8a61d56
Otavio L. de Giacometti <otaviolg2000@gmail.com>
2024-08-17 21:22:33 -0300

    Foi

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,8 @@
-// Módulo JavaScript
 export function exibirMensagem() {
   const mensagem = document.getElementById("mensagem");
   mensagem.textContent = "Mensagem alterada pelo JavaScript!";
 }
 
-// Chamando a função ao carregar a página
 document.addEventListener("DOMContentLoaded", () => {
   exibirMensagem();
 });
